b75985b83ac94abfc572e1dbd3d84ca4e181ba1a
Piyush <pmodak2209c@gmail.com>
2026-06-14 20:52:07 +0530

    Studied analogRead and map functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
 #include <Arduino.h>
-
-int ledState = 0;
-int lastTouchState = 0;  // 0 = not touched last time
-
 void setup() {
-    Serial.begin(115200);
-    pinMode(2, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
-    int currentTouch = touchRead(15) < 100;  // 1 if touching, 0 if not
-    
-    if(currentTouch == 1 && lastTouchState == 0) {
-        // NEW touch detected!
-        // toggle ledState — if 0 make 1, if 1 make 0
-        // write that to LED
-        ledState = 1-ledState;
-        digitalWrite(2,ledState);
-
-    }
-    
-    lastTouchState = currentTouch;  // save current for next time
-    delay(100);
+  int value = analogRead(34);
+  float voltage = value * (3.3 / 4095.0);
+  int percent = map(value, 0, 4095, 0, 100);
+  Serial.print("Raw: ");
+  Serial.print(value);
+  Serial.print("  |  Voltage: ");
+  Serial.print(voltage);
+  Serial.print("V  |  Percent: ");
+  Serial.print(percent);
+  Serial.println("%");
+  delay(200);   
 }
