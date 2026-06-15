451c108ed66fc5b1343c4242e6edc66f31755aaf
Piyush <pmodak2209c@gmail.com>
2026-06-15 16:46:39 +0530

    Add touch-controlled LED brightness with SMA filter

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include<Arduino.h>
+int readings[5] = {0,0,0,0,0};
+int inde = 0;
 void setup() {
-  Serial.begin(115200);
+Serial.begin(115200);
+pinMode(2,OUTPUT);
+ledcSetup(0,5000,8);
+ledcAttachPin(2,0);
+int firstReading = touchRead(4);
+for(int i = 0; i<5 ; i++) {
+  readings[i] = firstReading;
 }
-
+}
 void loop() {
-  int value = analogRead(34);
-  float voltage = value * (3.3 / 4095.0);
-  int percent = map(value, 0, 4095, 0, 100);
-  Serial.print("Raw: ");
-  Serial.print(value);
-  Serial.print("  |  Voltage: ");
-  Serial.print(voltage);
-  Serial.print("V  |  Percent: ");
-  Serial.print(percent);
-  Serial.println("%");
-  delay(200);   
+int tr = touchRead(4);
+readings[inde] = tr;
+inde=inde+1;
+if(inde==5)
+inde = 0;
+int total = 0;
+for(int i = 0; i<5 ; i++) {
+  total = total+readings[inde];
+}
+int avg = total/5;
+Serial.print("Raw: ");
+Serial.println(touchRead(4));
+Serial.print("Average: ");
+Serial.print(avg);
+int a = map(avg,116,0,0,255);
+ledcWrite(0,a);
+delay(200);
 }
